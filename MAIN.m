b086c9950c77bfe81f38612a81632669cc75975b
Thuy Nguyen <nguyentuongthuy@gmail.com>
2017-05-31 22:09:06 -0700

    Update MAIN.m
    Remove unnecessary comments.

--- MAIN.m
+++ MAIN.m
@@ -1,33 +1,16 @@
 close all;
 warning off;
-addpath('E:\\_Thuy\code\Matlab functions\toolbox\stats');
 
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% LOAD DATA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % BIOFILM DATA
-excelFile       = 'E:\\_Thuy\\project\\01-BioFilm\\paper\\1-for biofilm data\\130925\\714 samples 143x9 features.xlsx';
+excelFile       = '714 samples 143x9 features.xlsx';
 sheetNo         = 1;
 nColsFrom       = 1;
 nCols           = 1287;
 groupColIdx     = nCols + 1;
 nComponents     = 60;
-
-% MAMMOGRAPHIC MASS DATA
-% excelFile       = 'E:\\_Thuy\\project\\01-BioFilm\\paper\\2-handling missing data\\code\\mammographic.data\\4-mammographic.data (predicted missing values).xlsx';
-% sheetNo         = 1;
-% nColsFrom       = 2;
-% nCols           = 5;
-% groupColIdx     = nCols + 1;
-% nComponents     = 5;
-
-% PIMA INDIAN DIABETES DATA
-% excelFile       = 'E:\\_Thuy\\project\\01-BioFilm\\paper\\2-handling missing data\\code\\pima.data\\4-pima.data (predicted missing values).xlsx';
-% sheetNo         = 1;
-% nColsFrom       = 2;
-% nCols           = 8;
-% groupColIdx     = nCols + 1;
-% nComponents     = 5;
 
 
 % load and filter data
